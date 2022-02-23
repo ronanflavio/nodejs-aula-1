022b2b1ebadcd3d22ce54a39d538477c8f6049c5
Ronan Flavio <ronan.flavio@hotmail.com>
2022-02-22 22:03:12 -0300

    incluindo parâmetro heroku

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
 });
 
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 server.listen(port, hostname, () => {
     console.log(`Server running at: http://${hostname}:${port}`);
