3922af72c2cc3f8db73e65cf21e90165eba7d1ca
Ronan Flavio <ronan.flavio@hotmail.com>
2022-02-24 19:27:34 -0300

    utilizando express

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser')
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('Ronan Flavio Ribeiro');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.set('views', './src/views')
+app.set('view engine','ejs');
+
+app.use(bodyParser.urlencoded({ extended : true }))
+
+app.get('/', (req, res) => {
+    res.render('index');
 });
 
-const hostname = '127.0.0.1';
-const port = process.env.PORT;
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at: https://${hostname}:${port}`);
+app.listen(port, () => {
+    console.log(`Listen on port ${port}`)
 });
